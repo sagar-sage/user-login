74cd8180f27de8dd79da443dfc36331d98d6845a
Sagar Chhetri <sagar.chhetri@techguthi.com>
2016-01-18 11:40:45 +0545

    Initial Commit

---
+++ myapp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  myapp
+//
+//  Created by Sagar Chhetri on 1/18/16.
+//  Copyright © 2016 Sagar Chhetri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
